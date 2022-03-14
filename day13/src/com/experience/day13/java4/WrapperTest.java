7867f12bf14ffe4a05b7496d6c7083da030546fd
Free_Aaron.Li@outlook.com <Free_Aaron.Li@outlook.com>
2022-03-14 14:10:44 +0800

    增加基本数据类型转换为String类型

--- day13/src/com/experience/day13/java4/WrapperTest.java
+++ day13/src/com/experience/day13/java4/WrapperTest.java
@@ -4,25 +4,53 @@ import org.junit.Test;
  * @Description:
  * 包装类的使用：
  * 1.Java提供了8种基本数据类型对应的包装类，使得基本数据类型变量具有类的特征
- * 
- * 2.掌握:基本数据类型，包装类和String三者之间的转换 
+ *
+ * 2.掌握:基本数据类型，包装类和String三者之间的转换
  * @version: JDK17
  * @Author: Aaron.Li
  * @Date: 2022-03-11 17:20:27
- * @LastEditTime: 2022-03-11 18:37:26
+ * @LastEditTime: 2022-03-14 14:09:48
  */
 public class WrapperTest {
-//基本数据类型、包装类--->String类型
+    // 包装类--->基本数据类型：调用
+    // String类型--->基本数据类型、包装类：调用包装类的passXxx()
     @Test
-    public void testFour(){
-
+    public void testFive() {
+        String strOne = "123";
+        // 这是错误的
+        // int numOne=(int)strOne;
+        int numOne=Integer.parseInt(strOne);
+        System.out.println(numOne+1);
     }
+
+    // 基本数据类型、包装类--->String类型：调用String重载的valueOf(Xxx xxx)
+    @Test
+    public void testFour() {
+        int numOne = 10;
+        // 方式1：连接运算
+        String strOne = numOne + "";
+        System.out.println(strOne);
+        // 方式2：调用String的valueOf(Xxx xxx)
+        float floatOne = 12.3f;
+        String strTwo = String.valueOf(floatOne);
+        System.out.println(strTwo);
+
+        String strFour="true";
+        boolean booleanOne=Boolean.parseBoolean(strFour);
+        System.out.println(booleanOne);
+
+        Double doubleOne = new Double(12.4);
+        String strThree = String.valueOf(doubleOne);
+        System.out.println(strThree);// "12.4"
+    }
+
     /**
      * JDK 5.0新特性：自动装箱和自动拆箱
-     * 
+     *
      * @param obj
      */
-    @Test public void testThree() {
+    @Test
+    public void testThree() {
         // int numOne = 10;
         // // 基本数据类型--->包装类的对象
         // method(numOne);
