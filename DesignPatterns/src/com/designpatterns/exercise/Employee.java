60d11f02d6831bbd526aa60425750031b03c60f7
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-05-02 21:04:46 +0800

    设计模式：模板设计模式

---
+++ DesignPatterns/src/com/designpatterns/exercise/Employee.java
@@ -0,0 +1,12 @@
+package com.designpatterns.exercise;
+
+/**
+ * @projectName: newJavaProject
+ * @className: Employee
+ * @author: AaronLi
+ * @description: TODO
+ * @date: 2022/5/2 21:04
+ * @version: JDK17
+ */
+public class Employee {
+}
--- DesignPatterns/src/com/designpatterns/project/TemplateMethodTest.java
+++ DesignPatterns/src/com/designpatterns/project/TemplateMethodTest.java
@@ -1,4 +1,4 @@
-package com.designpatterns;
+package com.designpatterns.project;
 
 /**
  * @projectName: newJavaProject
