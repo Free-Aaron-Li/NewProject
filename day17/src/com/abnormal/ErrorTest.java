0fef73d1f3cba6fd6452f4e44645a88050eed1f0
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-05-14 18:23:48 +0800

    异常处理之Error

---
+++ day17/src/com/abnormal/ErrorTest.java
@@ -0,0 +1,20 @@
+package com.abnormal;
+
+/**
+ * @projectName: newJavaProject
+ * @className: ErrorTest
+ * @author: AaronLi
+ * @description:
+ * 关于Error异常的测试和查看
+ * @date: 2022/5/14 16:21
+ * @version: JDK17
+ */
+public class ErrorTest {
+    public static void main(String[] args) {
+        //1.Error,栈溢出 java.lang.StackOverflowError
+        //main(args);
+
+        //2.Error,堆溢出，内存不够 java.lang.OutOfMemoryError
+        //Integer[] arr=new Integer[1024*1024*1024];
+    }
+}
