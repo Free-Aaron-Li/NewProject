f83f689c6badf823f1416dbf57e6d30d56c5b247
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-05-15 16:45:10 +0800

    自定义异常MyException

--- day17/src/com/abnormal/test/StudentTest.java
+++ day17/src/com/abnormal/test/StudentTest.java
@@ -1,10 +1,13 @@
 package com.abnormal.test;
 
+import com.abnormal.MyException;
+
 /**
  * @projectName: newJavaProject
  * @className: StudentTest
  * @author: AaronLi
  * @description:
+ * 异常抛出方法throw和自定义异常MyException的调用
  * @date: 2022/5/15 15:15
  * @version: JDK17
  */
@@ -31,7 +34,8 @@ class Student {
 
             //手动抛出异常对象
             //throw new RuntimeException("非法");
-            throw new Exception("您输入的数据非法！");
+            //throw new Exception("您输入的数据非法！");
+            throw new MyException("不能输入负数！！");
         }
     }
 }
