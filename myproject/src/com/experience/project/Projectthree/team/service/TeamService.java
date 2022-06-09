8a60830669af064d35c0795fb19a7b1c9497f7c1
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-06-10 00:33:49 +0800

    更新 TeamService removeMember的添加

--- myproject/src/com/experience/project/Projectthree/team/service/TeamService.java
+++ myproject/src/com/experience/project/Projectthree/team/service/TeamService.java
@@ -58,15 +58,15 @@ public class TeamService {
     public void addMember(Employee employee) throws TeamException {
         //一、成员已满，无法添加
         if (total >= MAX_MEMBER) {
-            throw new TeamException("成员已满，无法添加!!");
+            throw new TeamException("成员已满，无法添加！！");
         }
         //二、该成员不是开发人员，无法添加
         if (!(employee instanceof Programmer)) {
-            throw new TeamException("该成员不是开发人员，无法添加!!");
+            throw new TeamException("该成员不是开发人员，无法添加！！");
         }
         //三、该员工已经在开发团队
         if (isExit(employee)) {
-            throw new TeamException("该员工已经在开发团队!!");
+            throw new TeamException("该员工已经在开发团队！！");
         }
         //四、该员工已是团队成员
         //五、该员工正在休假，无法添加
@@ -91,11 +91,11 @@ public class TeamService {
 
         //六、团队中至多只能有一名架构师        if(programmer instanceof Architect){
         if (numOfArch >= 1) {
-            throw new TeamException("团队中至多只能有一名架构师!!");
+            throw new TeamException("团队中至多只能有一名架构师！！");
         }
         //七、团队中至多只能有两名设计师
         else if (numOfDes >= 2) {
-            throw new TeamException("团队中至多只能有两名设计师!!");
+            throw new TeamException("团队中至多只能有两名设计师！！");
         }
         //八、团队中至多只能有三名程序员
         else if (numOfPro >= 3) {
@@ -105,7 +105,10 @@ public class TeamService {
         //九、将programmer添加到现有的team中
         team[total++] = programmer;
         //十、将programmer的属性赋值
+
+        //TeamID
         programmer.setMemberId(counter++);
+        //状态更改
         programmer.setStatus(Status.BUSY);
     }
 
@@ -130,6 +133,25 @@ public class TeamService {
      * @date: 2022/6/9 9:18
      * @description: 移除团队开发者
      */
-    public void removeMember(int memberId) {
+    public void removeMember(int memberId) throws TeamException {
+        int i = 0;
+        for (; i < total; i++) {
+            if (team[i].getMemberId() == memberId) {
+                //更改状态
+                team[i].setStatus(Status.FREE);
+                break;
+            }
+        }
+        //未找到指定memberId的情况
+        if(i==total){
+            throw new TeamException("找不到指定memberId的员工，删除失败！！");
+        }
+        //覆盖
+        for (int j = i = 1; j < total; j++) {
+            team[j - 1] = team[j];
+        }
+        //最后一个数组设置为null
+        team[--total] = null;
+
     }
 }
